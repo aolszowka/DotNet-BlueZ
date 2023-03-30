3a05731a3716c802b6aefedfe141a10e427c5185
vestervang <vestervang-rs@hotmail.com>
2023-03-30 20:58:04 +0200

    * Removed unused variable * Added console log to follow which devices has been found

--- manualTests/DotNetBlueZTest1/Program.cs
+++ manualTests/DotNetBlueZTest1/Program.cs
@@ -25,29 +25,25 @@ internal class Program
         Console.WriteLine($"Scanning for {scanSeconds} seconds...");
 
         adapter.DeviceFound += DeviceFound;
-            
+
         var props = new Dictionary<string, object>();
         props.Add("Transport", "le");
         props.Add("DuplicateData", false);
         await adapter.SetDiscoveryFilterAsync(props);
-            
+
         await adapter.StartDiscoveryAsync();
         await Task.Delay(TimeSpan.FromSeconds(scanSeconds));
         await adapter.StopDiscoveryAsync();
 
         var devices = await adapter.GetDevicesAsync();
         Console.WriteLine($"{devices.Count} device(s) found.");
-            
     }
 
     private static async Task DeviceFound(Adapter sender, DeviceFoundEventArgs eventArgs)
     {
         var deviceProperties = await eventArgs.Device.GetAllAsync();
-        var device = eventArgs.Device;
-        var deviceName = await device.GetNameAsync();
-        var deviceAddress = await device.GetAddressAsync();
 
-
-        var i = 0;
+        Console.WriteLine(
+            $"[{DateTime.UtcNow}] Found device {deviceProperties.Name} with address {deviceProperties.Address}");
     }
 }
